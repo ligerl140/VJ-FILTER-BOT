e0191f44c8f7ee654844a76fe48ac3f818d71814
ligerl140 <ligerl140@gmail.com>
2025-01-06 18:38:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ PICS = (environ.get('PICS', 'https://vault.pictures/p/7430e2d333244b94a23648e0d9
 # Admins & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6398757880').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5905976120').split()]  # For Multiple Id Use One Space Between Each.
-AUTH_USERS = (auth_users + ADMINS) if auth_users else [@HatakeKakashi41]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else [6398757880]
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002316391931'))
